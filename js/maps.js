2d10c1a8aa1bc79f568318f4f7490e50bcbfca36
Eleonora <eleonora.smid@gmail.com>
2016-03-17 12:46:29 +0100

    Changing the map layout to fit in /testest

--- js/maps.js
+++ js/maps.js
@@ -1,14 +1,107 @@
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 0, lng: 0},
-    zoom: 3,
-    styles: [{
-      featureType: 'poi',
-      stylers: [{ visibility: 'off' }]  // Turn off points of interest.
-    }, {
-      featureType: 'transit.station',
-      stylers: [{ visibility: 'off' }]  // Turn off bus stations, train stations, etc.
-    }],
-    disableDoubleClickZoom: true
+      var map;
+      function initMap() {
+        var styles = [
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#444444"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.country",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            },
+            {
+                "hue": "#ffd600"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f0db4f"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    }
+];
+ var image = 'http://nljs.nl/apple-icon-60x60.png';
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 52.3773657, lng: 4.8876505},
+          zoom: 16
+        });
+  var Marker = new google.maps.Marker({
+    position: {lat: 52.3773657, lng: 4.8876505},
+    map: map,
+    icon: image
   });
-}
+
+           map.setOptions({styles: styles});
+      }
